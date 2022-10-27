39a238033b6b677730c3473a120ca494e6d61e10
danangwisnup <danangwisnu@students.undip.ac.id>
2022-10-27 20:20:41 +0700

    update result form

--- app/src/main/java/com/example/tugas_pbp_kel4_formlogin/loginActivity.java
+++ app/src/main/java/com/example/tugas_pbp_kel4_formlogin/loginActivity.java
@@ -17,19 +17,30 @@ public class loginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-//
-//        Resultnama = String.valueOf((TextView)findViewById(R.id.Rnama_form));
-//        Resulttempat_lahir = String.valueOf((TextView)findViewById(R.id.Rtempat_lahir));
-//        Resulttgl_lahir = String.valueOf((TextView)findViewById(R.id.Rtgl_lahir));
-//
-//        Bundle b = getIntent().getExtras();
-//
-//        Resultnama = b.putString("parse_nama");
-//        Resulttempat_lahir = b.putString("parse_tempat_lahir");
-//        Resulttgl_lahir = b.putString("parse_tgl_lahir");
-//
-//        Rnama_form.setText("");
-//        Rtempat_lahir.setText("");
-//        Rtgl_lahir.setText("");
+
+        Rnama_form = (TextView)findViewById(R.id.Rnama_form);
+        Rtempat_lahir = (TextView)findViewById(R.id.Rtempat_lahir);
+        Rtgl_lahir = (TextView)findViewById(R.id.Rtgl_lahir);
+
+        Bundle b = getIntent().getExtras();
+
+        Resultnama = b.getString("parse_nama");
+        Resulttempat_lahir = b.getString("parse_tempat_lahir");
+        Resulttgl_lahir = b.getString("parse_tgl_lahir");
+
+        Rnama_form.setText(""+Resultnama);
+        Rtempat_lahir.setText(""+Resulttempat_lahir);
+        Rtgl_lahir.setText(""+Resulttgl_lahir);
+
+        Button back = (Button) findViewById(R.id.back);
+        back.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = null;
+                i = new Intent(loginActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
+
 }
