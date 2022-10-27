332548207d0ad05ecb5021eab7754b9a153a6e9e
danangwisnup <danangwisnu@students.undip.ac.id>
2022-10-27 19:53:13 +0700

    update tampilan result

--- app/src/main/java/com/example/tugas_pbp_kel4_formlogin/MainActivity.java
+++ app/src/main/java/com/example/tugas_pbp_kel4_formlogin/MainActivity.java
@@ -3,19 +3,45 @@ package com.example.tugas_pbp_kel4_formlogin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    String nama_form, tempat_lahir, tgl_lahir;
+    EditText Inputnama, Inputtempat_lahir, Inputtgl_lahir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText nama_form = findViewById(R.id.nama_form);
-        EditText email_form = findViewById(R.id.tempat_lahir);
-        EditText tgl_lahir = findViewById(R.id.tgl_lahir);
-        Button login = findViewById(R.id.regis);
+        Inputnama = (EditText)findViewById(R.id.nama_form);
+        Inputtempat_lahir = (EditText)findViewById(R.id.tempat_lahir);
+        Inputtgl_lahir = (EditText)findViewById(R.id.tgl_lahir);
+
+        Button submit = (Button)findViewById(R.id.regis);
+        submit.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Inputnama.setText("Nama");
+//                Inputtempat_lahir.setText("Semarang");
+//                Inputtgl_lahir.setText("18-10-2022");
+                nama_form = Inputnama.getText().toString();
+                tempat_lahir = Inputtempat_lahir.getText().toString();
+                tgl_lahir = Inputtgl_lahir.getText().toString();
+                Intent i = null;
+                i = new Intent(MainActivity.this, loginActivity.class);
+                Bundle b = new Bundle();
+                b.putString("parse_nama",nama_form);
+                b.putString("parse_tempat_lahir",tempat_lahir);
+                b.putString("parse_tgl_lahir",tgl_lahir);
+
+                i.putExtras(b);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/tugas_pbp_kel4_formlogin/loginActivity.java
+++ app/src/main/java/com/example/tugas_pbp_kel4_formlogin/loginActivity.java
@@ -2,13 +2,34 @@ package com.example.tugas_pbp_kel4_formlogin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class loginActivity extends AppCompatActivity {
+    TextView Rnama_form, Rtempat_lahir, Rtgl_lahir;
+    String Resultnama, Resulttempat_lahir, Resulttgl_lahir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+//
+//        Resultnama = String.valueOf((TextView)findViewById(R.id.Rnama_form));
+//        Resulttempat_lahir = String.valueOf((TextView)findViewById(R.id.Rtempat_lahir));
+//        Resulttgl_lahir = String.valueOf((TextView)findViewById(R.id.Rtgl_lahir));
+//
+//        Bundle b = getIntent().getExtras();
+//
+//        Resultnama = b.putString("parse_nama");
+//        Resulttempat_lahir = b.putString("parse_tempat_lahir");
+//        Resulttgl_lahir = b.putString("parse_tgl_lahir");
+//
+//        Rnama_form.setText("");
+//        Rtempat_lahir.setText("");
+//        Rtgl_lahir.setText("");
     }
 }
