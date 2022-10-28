c5fe8a9c7b2b765e81668f499cde6f3c20444a12
rijalkwn <rijalkurniawan@students.undip.ac.id>
2022-10-28 10:56:12 +0700

    update input tanggal

--- app/src/main/java/com/example/tugas_pbp_kel4_formlogin/MainActivity.java
+++ app/src/main/java/com/example/tugas_pbp_kel4_formlogin/MainActivity.java
@@ -2,19 +2,29 @@ package com.example.tugas_pbp_kel4_formlogin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.content.Intent;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.Calendar;
+import java.util.Calendar;
 public class MainActivity extends AppCompatActivity {
     String nama_form, tempat_lahir, tgl_lahir;
     EditText Inputnama, Inputtempat_lahir, Inputtgl_lahir;
     TextView rError;
+    private static final String TAG = "MainActivity";
+    private TextView mDisplayDate;
+    private DatePickerDialog.OnDateSetListener mDataSetListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +36,30 @@ public class MainActivity extends AppCompatActivity {
         Inputtgl_lahir = (EditText)findViewById(R.id.tgl_lahir);
         rError = (TextView) findViewById(R.id.result_error);
 
+        mDisplayDate = (EditText) findViewById(R.id.tgl_lahir);
+        mDisplayDate.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(MainActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDataSetListener, year, month, day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+        mDataSetListener = new DatePickerDialog.OnDateSetListener(){
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day){
+                Log.d(TAG, "onDateSet: mm/dd/yyyy: "+month+"/"+day+"/"+year);
+                String date = month + "/" + day + "/" + year;
+                mDisplayDate.setText(date);
+            }
+        };
         Button submit = (Button)findViewById(R.id.regis);
         submit.setOnClickListener(new OnClickListener() {
             @Override
